5c6f139d8748416faae66adaa7c6a26e10162e02
Austin <austinjrosenbaum@gmail.com>
2019-02-11 10:10:18 -0800

    added signed binary representation

--- fimath.py
+++ fimath.py
@@ -26,11 +26,11 @@ class Fi:
                 conv_signed = True
                 signed = True
                 # raise ValueError("Unsigned value cannot be negative")
-            elif value >= 2**(n_bits - n_fbits):
+            elif value >= 2 ** (n_bits - n_fbits):
                 raise ValueError(f"Value is too large to be represented with {n_bits} integer bits "
                                  f"and {n_fbits} fractional bits")
 
-        if signed and ((-(2**(n_bits - n_fbits - 1))) > value or value >= 2**(n_bits - n_fbits - 1)):
+        if signed and ((-(2 ** (n_bits - n_fbits - 1))) > value or value >= 2 ** (n_bits - n_fbits - 1)):
             if conv_signed:
                 print("Failed to convert value to signed")
 
@@ -49,7 +49,7 @@ class Fi:
             print(f"{key} : {kwargs[key]}")
 
     def bin(self, binary_point_on=True, grouping=0):
-        # TODO: Input validation
+        # TODO: Add negative value support
 
         if not isinstance(binary_point_on, bool):
             raise TypeError("Argument 'binary_point_on' must be of type 'bool'")
@@ -61,7 +61,9 @@ class Fi:
             raise ValueError("Grouping cannot be negative")
 
         # Return the floating point binary representation of self.value
-        temp = self.value
+
+        temp = abs(self.value)
+
         output = "0b"
         for i in range(self.n_bits):
             if binary_point_on and i == (self.n_bits - self.n_fbits):
@@ -71,13 +73,63 @@ class Fi:
                     output += " "
 
             exp = self.n_bits - self.n_fbits - i - 1
-            if temp >= 2**exp:
+            if temp >= 2 ** exp:
                 output += "1"
-                temp -= 2**exp
+                temp -= 2 ** exp
             else:
                 output += "0"
 
+        if self.value < 0:
+            output = self.twos_comp(output, binary_point_on, grouping)
+
         return output
+
+    # return the two's complement of the object's value
+    def twos_comp(self, binary_string, binary_point_on, grouping):
+        binary_string_mod = binary_string
+        # remove prefix
+        binary_string_mod = binary_string_mod[2:]
+
+        # remove binary point
+        use_binary_point = False
+        binary_point_loc = None
+        if binary_point_on and "." in binary_string_mod:
+            use_binary_point = True
+            binary_point_loc = binary_string_mod.index(".")
+            binary_string_mod = "".join(binary_string_mod.split("."))
+
+        # remove spaces
+        if grouping > 0:
+            binary_string_mod = "".join(binary_string_mod.split(" "))
+
+        # invert every bit
+        twos_comp_string = ""
+        for b in binary_string_mod:
+            if b == "0":
+                twos_comp_string += "1"
+            else:
+                twos_comp_string += "0"
+
+        # Add one to the lsb
+        count = self.n_bits - 1
+        while count >= 0:
+            if twos_comp_string[count] == "0":
+                twos_comp_string = twos_comp_string[:count] + "1" + twos_comp_string[count+1:]
+                break
+            else:
+                twos_comp_string = twos_comp_string[:count] + "0" + twos_comp_string[count+1:]
+                count -= 1
+
+        # Add spacing
+
+        # add binary point
+        if use_binary_point:
+            twos_comp_string = twos_comp_string[:binary_point_loc] + "." + twos_comp_string[binary_point_loc:]
+
+        # Add prefix back
+        twos_comp_string = "0b" + twos_comp_string
+
+        return twos_comp_string
 
     # TODO: Define builtin functions using Python Data Model (CH 3)
 
